caa83d6448c6f5ac6eb2b79aa3f4638315bb9750
poomchaiuds <109953357+poomchaiuds@users.noreply.github.com>
2024-07-30 00:32:01 +0700

    Add files via upload

--- src/lib/models.tsx
+++ src/lib/models.tsx
@@ -4,4 +4,20 @@ export interface Book {
   author: string;
   year: number;
   is_published: boolean;
+  description: string;
+  sub: string;
+  category: string;
+}
+
+export interface Coffee {
+  c_id: number;
+  c_title: string;
+  c_price: number;
+}
+
+export interface Order {
+  id: number;
+  menu: string;
+  total: number;
+  note: string;
 }
